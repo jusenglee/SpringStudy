469747d306e4f18e0dc0a2e7e1a6f3a78c61a566
Justin lee <85321903+jusenglee@users.noreply.github.com>
2023-05-09 11:12:08 +0900

    Delete src directory

--- src/main/java/fighting/spirit/springstudy/repository/AnswerRepository.java
+++
@@ -1,9 +0,0 @@
-package fighting.spirit.springstudy.repository;
-
-import fighting.spirit.springstudy.entity.AnswerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface AnswerRepository extends JpaRepository<AnswerEntity, Integer> {
-
-}
